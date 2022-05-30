605d85a4506dd94c9502b33942a1cd1a26a3a81d
Thomas Cholley <thomas.cholley@appenin.fr>
2022-05-30 08:42:38 +0200

    node-typescript-compiled add xo linter

---
+++ node-typescript-compiled/.xo-config.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+	envs: ['es2021', 'node'],
+	prettier: true,
+};
--- node-typescript-compiled/src/index.ts
+++ node-typescript-compiled/src/index.ts
@@ -1,4 +1,6 @@
+import process from 'node:process';
+
 export function main(): void {
-	const example: number = 42;
+	const example = 42;
 	process.stdout.write(example.toString());
 }
